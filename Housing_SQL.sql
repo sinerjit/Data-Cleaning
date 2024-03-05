0322e64b80b2174f58824697fffbc543de36d17a
Sinerjit <sarp.cubukcuoglu@gmail.com>
2024-03-04 19:48:00 -0800

    Update SQL

--- Housing_SQL.sql
+++ Housing_SQL.sql
@@ -14,9 +14,20 @@ From housing
 ORDER BY "ParcelID"
 
 
-SELECT a."ParcelID", a."PropertyAddress", b."ParcelID", b."PropertyAddress"
-FROM housing as a
-JOIN housing as b
+SELECT a."ParcelID", a."PropertyAddress", b."ParcelID", b."PropertyAddress", COALESCE(a."PropertyAddress", b."PropertyAddress") AS "MergedPropertyAddress"
+FROM housing AS a
+JOIN housing AS b
 ON a."ParcelID" = b."ParcelID"
 AND a."UniqueID " <> b."UniqueID "
-WHERE a."PropertyAddress" is NULL;
+WHERE a."PropertyAddress" IS NULL;
+
+
+UPDATE housing AS a
+SET "PropertyAddress" = CASE
+   WHEN a."PropertyAddress" IS NULL THEN b."PropertyAddress"
+   ELSE a."PropertyAddress"
+END
+FROM housing AS b
+WHERE a."ParcelID" = b."ParcelID"
+  AND a."UniqueID " <> b."UniqueID "
+  AND a."PropertyAddress" IS NULL;
