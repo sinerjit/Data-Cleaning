6c92d02e0734d80a66d811c17ffe842ebc8e6dd1
Sinerjit <sarp.cubukcuoglu@gmail.com>
2024-03-08 15:48:59 -0800

    Update SQL

--- DataCleaning.sql
+++ DataCleaning.sql
@@ -1,18 +1,18 @@
-Select "SaleDate"
-From housing
-
--- Populate Property Address Data --
-Select "PropertyAddress"
-From housing
-WHERE "PropertyAddress" is null
+select *
+FROM housing
 
 
+-- Standardize Date Format --
+ALTER TABLE housing
+ALTER COLUMN "SaleDate" TYPE DATE USING TO_DATE("SaleDate", 'FMMonth DD, YYYY');
+
+
+-- Populate Property Address data --
 Select *
 From housing
 -- WHERE "PropertyAddress" is null
 ORDER BY "ParcelID"
 
--- Find Nulls --
 SELECT a."ParcelID", a."PropertyAddress", b."ParcelID", b."PropertyAddress", COALESCE(a."PropertyAddress", b."PropertyAddress") AS "MergedPropertyAddress"
 FROM housing AS a
 JOIN housing AS b
@@ -20,7 +20,6 @@ ON a."ParcelID" = b."ParcelID"
 AND a."UniqueID " <> b."UniqueID "
 WHERE a."PropertyAddress" IS NULL;
 
--- Update the Nulls --
 UPDATE housing AS a
 SET "PropertyAddress" = CASE
    WHEN a."PropertyAddress" IS NULL THEN b."PropertyAddress"
@@ -30,9 +29,9 @@ FROM housing AS b
 WHERE a."ParcelID" = b."ParcelID"
   AND a."UniqueID " <> b."UniqueID "
   AND a."PropertyAddress" IS NULL;
-  
--- Breaking out Address into Individual Columns (Address, City, State) --
 
+
+-- Breaking out Address into Individual Columns (Address, City, State) --
 Select "PropertyAddress"
 From housing
 -- WHERE "PropertyAddress" is null
@@ -59,79 +58,53 @@ SET SplitCity = SUBSTRING("PropertyAddress", POSITION(',' IN "PropertyAddress") 
 SELECT *
 FROM housing;
 
-
----------------------------
-
 SELECT "OwnerAddress"
 FROM housing;
 
--- PARSENAME / Split_Part() --
 SELECT
 split_part("OwnerAddress", ',', 1) AS Address,
 split_part("OwnerAddress", ',', 2) AS City,
 split_part("OwnerAddress", ',', 3) AS State
 FROM housing;
 
-
-ALTER TABLE housing
-Add OwnerSplitAddress VARCHAR(255);
-
-UPDATE housing
-SET OwnerSplitAddress = split_part("OwnerAddress", ',', 1)
-
-ALTER TABLE housing
-Add OwnerSplitCity VARCHAR(255);
-
-UPDATE housing
-SET OwnerSplitCity = split_part("OwnerAddress", ',', 2)
-
-ALTER TABLE housing
-Add OwnerSplitState VARCHAR(255);
-
-UPDATE housing
-SET OwnerSplitState = split_part("OwnerAddress", ',', 3)
-/* More Cleaner writing style
--- Add new columns
 ALTER TABLE housing
 ADD COLUMN OwnerSplitAddress VARCHAR(255),
 ADD COLUMN OwnerSplitCity VARCHAR(255),
 ADD COLUMN OwnerSplitState VARCHAR(255);
 
--- Update data to new columns
 UPDATE housing
-SET 
+SET
   OwnerSplitAddress = split_part("OwnerAddress", ',', 1),
   OwnerSplitCity = split_part("OwnerAddress", ',', 2),
   OwnerSplitState = split_part("OwnerAddress", ',', 3);
-*/
+
+SELECT *
+FROM housing;
 
 
--- Change Y and N toYes and No in "Sold as Vacant" field --
-
+-- Change Y and N to Yes and No in "Sold as Vacant" field --
 SELECT DISTINCT("SoldAsVacant"), COUNT("SoldAsVacant")
 FROM housing
 GROUP BY "SoldAsVacant"
 ORDER BY 2;
 
-
 SELECT "SoldAsVacant",
-CASE 
+CASE
 	WHEN "SoldAsVacant" = 'Y' THEN 'Yes'
-	WHEN "SoldAsVacant" = 'N' THEN 'No' 
+	WHEN "SoldAsVacant" = 'N' THEN 'No'
 	ELSE "SoldAsVacant"
 	END
 FROM housing;
 
-
 UPDATE housing
-SET "SoldAsVacant" = CASE 
+SET "SoldAsVacant" = CASE
 	WHEN "SoldAsVacant" = 'Y' THEN 'Yes'
-	WHEN "SoldAsVacant" = 'N' THEN 'No' 
+	WHEN "SoldAsVacant" = 'N' THEN 'No'
 	ELSE "SoldAsVacant"
 	END
 
--- Remove Duplicates --
 
+-- Remove Duplicates --
 WITH RowNumCTE AS(
 Select *,
 	ROW_NUMBER() OVER (
@@ -153,7 +126,6 @@ Where row_num > 1
 --Order by "PropertyAddress"
 
 -- DELETE --
-
 WITH RowNumCTE AS (
     SELECT *,
         ROW_NUMBER() OVER (
@@ -176,8 +148,8 @@ WHERE ("ParcelID", "PropertyAddress", "SalePrice", "SaleDate", "LegalReference",
 Select *
 From housing
 
--- Delete Unused Columns -- 
 
+-- Delete Unused Columns --
 Select *
 From housing;
 
@@ -186,6 +158,3 @@ ALTER TABLE housing
 DROP COLUMN "OwnerAddress",
 DROP COLUMN "TaxDistrict",
 DROP COLUMN "PropertyAddress";
-
-
-
